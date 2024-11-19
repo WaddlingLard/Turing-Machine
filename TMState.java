0423c3ea133b913790e1ea8e12e3cf6a65b7db3c
Brian <dathamor@gmail.com>
2024-11-19 13:28:37 -0700

    TMState partially completed

--- TMState.java
+++ TMState.java
@@ -1,4 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TMState {
     
+    private int element;
+
+    // Could use TMState to be stored in HashMap instead
+    private Map<Integer, String> transitions;
+
+    public TMState() {
+        this.transitions = new HashMap<>();
+    }
+
+    public TMState(int element) {
+        this();
+        this.element = element;
+    }
+
+    public void setElement(int element) {
+        this.element = element;
+    }
+
+    public String addTransition(int nextState, String direction) {
+
+        return this.transitions.put(nextState, direction);
+    }
+
 }
