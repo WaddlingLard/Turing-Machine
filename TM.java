829cb5c2fceb84d84b68baf7024a76df9b7f2103
Max Ma <maxma@u.boisestate.edu>
2024-11-19 00:34:02 -0700

    TM variable and constructor initialization

--- TM.java
+++ TM.java
@@ -1,15 +1,32 @@
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class TM {
 
     private LinkedList<Integer> tape;
+    private int head; //head's current position 
+    private int state; // current state
+    // number of states and symbols for validation
+    private int numStates;
+    private int numSymbols; 
+    private Map<String, String> transitions;
 
-    public TM() {
+    public TM(int numStates, int numSymbols) {
         this.tape = new LinkedList<>();
-    }
-
-    public TM(String input) {
+        this.head =0;
+        this.state =0;
+        this.transitions = new HashMap<>();
+        this.numStates = numStates;
+        this.numSymbols = numSymbols;
         
     }
+    // constructor for Turing machine with input string 
+    public TM(String input, int numStates, int numSymbols) {
+        this(numStates, numSymbols);
+
+    }
+
+    
 
 }
